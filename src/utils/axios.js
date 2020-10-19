7f924947a21c2fbf9cf789b4bef3bd4195f35562
chenpeng <13488859404@163.com>
2020-10-19 17:08:26 +0800

    添加路由拦截与接口地址管理文件

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,10 +5,19 @@ axios.interceptors.response.use(
   res => {
     return res.data
   }, err => {
-    message.error('未登录, 请先登入');
-    setTimeout(()=>{
-      window.location.href='/login'
-    }, 1500)
+    console.log(err.response)
+    const {status: statusCode, data, statusText} = err.response
+    if(statusCode===401){
+      message.error('未登录, 请先登入');
+      setTimeout(()=>{
+        window.location.href='/login'
+      }, 1500)
+    } else if(statusCode===400){
+      message.error(`${statusCode}: ${statusText}`);
+    }else {
+      message.error(data.message);
+    }
+    return err
   }
 );
 export { axios }
---
+++ src/utils/httpUrl.config.js
@@ -0,0 +1,5 @@
+const BASE_URL="/ota-api"
+export let rolePath={
+  createRolePath: `${BASE_URL}/permission/role`,
+  department: `${BASE_URL}/permission/department`,
+}
